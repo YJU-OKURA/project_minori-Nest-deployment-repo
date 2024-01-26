e1c234a203792249b19a3e30781f7d92c304bbe9
Z00One <9000248@g.yju.ac.kr>
2024-01-26 20:40:34 +0900

    feat: Implement winstonLogger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
