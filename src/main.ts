06622d169bad81aed033f90726fceb7d3c7ab837
Z00One <9000248@g.yju.ac.kr>
2024-01-27 18:13:27 +0900

    feat: Apply swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonLogger } from './utils/winston.util';
+import { swagger } from './utils/swagger.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: winstonLogger,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  swagger(app);
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
