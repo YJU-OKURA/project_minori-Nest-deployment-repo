9fc94c0f4be25774bf56f6e2aeb0f3331a128499
Z00One <9000248@g.yju.ac.kr>
2024-02-15 21:25:53 +0900

    ✨ feat: Add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { winstonLogger } from './utils/winston.util';
-import { swagger } from './utils/swagger.util';
+import { winstonLogger } from './common/utils/winston.util';
+import { swagger } from './common/utils/swagger.util';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   winstonLogger(app);
   swagger(app);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
