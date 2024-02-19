ae3291470d6ee889a9a62c2f132c4aee87893007
Z00One <9000248@g.yju.ac.kr>
2024-02-19 19:23:43 +0900

    ✨ feat: Implement PrismaService and register in PrismaModule
    - Create PrismaService extending PrismaClient
    - Register PrismaService in PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import config from './common/configs/config';
 
 @Module({
@@ -16,6 +17,7 @@ import config from './common/configs/config';
       isGlobal: true,
       load: [config],
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService, Logger],
