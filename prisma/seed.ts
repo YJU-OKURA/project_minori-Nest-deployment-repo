db3e3ed00c49d904831da4758ba92bedac6eea3d
Z00One <9000248@g.yju.ac.kr>
2024-02-19 18:52:00 +0900

    ✨ feat: Implement prisma seeding file
    - create class_user for test of authentication

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.class_users.deleteMany();
+
+  console.log('Seeding...');
+
+  const user1 = await prisma.class_users.create({
+    data: {
+      u_id: 1,
+      c_id: 1,
+      nickname: 'user1',
+      role: 'USER',
+      is_favorite: true,
+    },
+  });
+  const user2 = await prisma.class_users.create({
+    data: {
+      u_id: 2,
+      c_id: 1,
+      nickname: 'user2',
+      role: 'ADMIN',
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
