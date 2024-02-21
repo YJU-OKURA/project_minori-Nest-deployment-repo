c5602a1f8f9c9062125bf5d99e245ba1d62dc219
Z00One <9000248@g.yju.ac.kr>
2024-02-21 14:10:21 +0900

    ✨ feat: Configure AuthModule with PrismaModule and JwtModule
    JwtModule is configured asynchronously using ConfigService. AuthService is also provided and exported by this module.
    
    #29

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { SecurityConfig } from 'src/common/configs/config.interface';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        const securityConfig =
+          configService.get<SecurityConfig>('security');
+        return {
+          secret: securityConfig.secret,
+          signOptions: {
+            expiresIn: securityConfig.expiresIn,
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
