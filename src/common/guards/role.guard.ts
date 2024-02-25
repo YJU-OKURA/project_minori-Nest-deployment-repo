c7cefcbd3cd594d0df400ecb9ad863b460aa08fc
Z00One <9000248@g.yju.ac.kr>
2024-02-25 23:18:03 +0900

    🐛 fix: Fix role guard userId and classId assignment
    - Changed the assignment of userId and classId in the role guard to correctly extract values from the request object
    - Removed unnecessary client and c_id checks

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -33,23 +33,20 @@ export class RolesGuard implements CanActivate {
       return true;
     }
 
-    const client = request['body']['client'];
-    const c_id = request.query['c_id'];
+    const userId = request['user'];
+    const classId = request.params['c_id'];
 
-    if (!client || !client['id'] || !c_id) {
+    if (!userId || !classId) {
       throw new UnauthorizedException(
         'Invalid user or class id.',
       );
     }
 
-    const u_id = BigInt(client['id']);
-    const classId = BigInt(c_id as string);
+    const u_id = BigInt(userId);
+    const c_id = BigInt(classId as string);
 
     const classUser =
-      await this.authService.getClassUserInfo(
-        u_id,
-        classId,
-      );
+      await this.authService.getClassUserInfo(u_id, c_id);
 
     if (!classUser) {
       throw new UnauthorizedException(
