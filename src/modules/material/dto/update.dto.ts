4e5a844300197d911c969a9a0a00c60c3e51a7e2
Z00One <9000248@g.yju.ac.kr>
2024-02-25 23:33:30 +0900

    ✨ feat: Implement UpdateMaterialDto
    #42

---
+++ src/modules/material/dto/update.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class UpdateMaterialDto {
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    description: '教材の名前',
+    required: false,
+  })
+  name: string;
+
+  @ApiProperty({
+    description: '300KB以下のPDFファイル',
+    required: false,
+  })
+  file: Express.Multer.File;
+}
