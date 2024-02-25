552e3b21bf12ec9bfca69ec64189c1dc57ad1d6c
Z00One <9000248@g.yju.ac.kr>
2024-02-25 23:42:30 +0900

    🐛 fix: remove unnecessary import
    - Removed ConfigModule import from upload.module.ts as it was unnecessary
    
    #42

--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule],
   providers: [UploadService],
   exports: [UploadService],
 })
