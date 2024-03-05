82fc3d7967320a11d9826bc3393c3a311f1d8642
Z00One <9000248@g.yju.ac.kr>
2024-03-05 22:19:41 +0900

    ✨ feat: Implement User Param Decorator
    Created a custom parameter decorator 'User' to extract and convert the user ID from the request object into a BigInt.

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,18 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    try {
+      return BigInt(request.user);
+    } catch {
+      throw new InternalServerErrorException(
+        'User ID is not valid.',
+      );
+    }
+  },
+);
