24abca2e416ab754c66fcc29345c85d3a37cee9d
Z00One <9000248@g.yju.ac.kr>
2024-03-19 15:53:26 +0900

    ✨ feat: Implement MaterialFeedbackModule
    #50

---
+++ src/modules/material-feedback/material-feedback.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { MaterialFeedbackController } from './material-feedback.controller';
+import { MaterialFeedbackService } from './material-feedback.service';
+import { PrismaModule } from '@modules/prisma/prisma.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { MaterialFeedbackRepository } from './material-feedback.repository';
+import { ReferModule } from '@modules/refer/refer.module';
+import { LangchainModule } from './langchain/langchain.module';
+import { FileModule } from './file/file.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ReferModule,
+    LangchainModule,
+    FileModule,
+  ],
+  controllers: [MaterialFeedbackController],
+  providers: [
+    MaterialFeedbackService,
+    MaterialFeedbackRepository,
+  ],
+})
+export class MaterialFeedbackModule {}
