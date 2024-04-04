da646bf7adb4c061dcf86e74d8ce3827b44d7068
Z00One <9000248@g.yju.ac.kr>
2024-04-04 13:32:28 +0900

    ♻️ refactor: add metadata decorator for use-owner-guards
    - Added metadata decorator for use-owner-guards to handle model and id parameters

--- src/common/decorators/use-owner-guards.decorator.ts
+++ src/common/decorators/use-owner-guards.decorator.ts
@@ -1,12 +1,16 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { ApiForbiddenResponse } from '@nestjs/swagger';
-import { Models } from './model.decorator';
 import { OwnerGuard } from '@common/guards/owner.guard';
+import { Key, MetaData } from './metadata.decorator';
 
-export const UseOwnerGuards = (model: Prisma.ModelName) => {
+export const UseOwnerGuards = (
+  model: Prisma.ModelName,
+  id: string = 'id',
+) => {
   return applyDecorators(
-    Models(model),
+    MetaData<Prisma.ModelName>(Key.MODELS, model),
+    MetaData<string>(Key.ID_PARAM, id),
     UseGuards(OwnerGuard),
     ApiForbiddenResponse({
       description: 'このアクセスする権限がありません。',
