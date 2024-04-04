717f6ff50b90fa08df8e3a6de468fafcc1b70427
Z00One <9000248@g.yju.ac.kr>
2024-04-04 20:45:51 +0900

    ✨ feat: Implemt QuizController
    #60

---
+++ src/modules/quiz/quiz.controller.ts
@@ -0,0 +1,95 @@
+import { ApiResponseWithBody } from '@common/decorators/api-response.decorator';
+import { UseRoleGuards } from '@common/decorators/use-role-guards.decorator';
+import { BigIntPipe } from '@common/pipes/bigint.pipe';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { QuizService } from './quiz.service';
+import { ApiDefaultMetadata } from '@common/decorators/api-default.decorator';
+import { CreateUpdateQuizDto } from './dto/create-update.dto';
+import { QuizEntity } from './entity/quiz.entity';
+
+@ApiDefaultMetadata('Quizzes')
+@Controller('quizzes')
+export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
+
+  @ApiResponseWithBody(
+    HttpStatus.OK,
+    'クイズ取得',
+    'クイズ取得に成功しました。',
+    QuizEntity,
+    true,
+  )
+  @UseRoleGuards()
+  @Get('material/:m_id')
+  getByMid(
+    @Param('m_id', BigIntPipe) m_id: bigint,
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    return this.quizService.getByMid(m_id, page, limit);
+  }
+
+  @ApiResponseWithBody(
+    HttpStatus.CREATED,
+    'クイズ作成',
+    'クイズ作成に成功しました。',
+    String,
+  )
+  @UseRoleGuards([Role.ADMIN])
+  @Post('material/:m_id')
+  create(
+    @Param('m_id', BigIntPipe) m_id: bigint,
+    @Body() body: CreateUpdateQuizDto,
+  ) {
+    return this.quizService.create(m_id, body.content);
+  }
+
+  @ApiResponseWithBody(
+    HttpStatus.OK,
+    'クイズ取得',
+    'クイズ取得に成功しました。',
+    CreateUpdateQuizDto,
+  )
+  @UseRoleGuards([Role.ADMIN])
+  @Get('material/:m_id/get-quiz')
+  getQuizByLLM(@Param('m_id', BigIntPipe) m_id: bigint) {
+    return this.quizService.getQuizByLLM(m_id);
+  }
+
+  @ApiResponseWithBody(
+    HttpStatus.CREATED,
+    'クイズ修正',
+    'クイズ修正に成功しました。',
+  )
+  @UseRoleGuards([Role.ADMIN])
+  @Patch(':q_id')
+  update(
+    @Param('q_id', BigIntPipe) q_id: bigint,
+    @Body() content: CreateUpdateQuizDto,
+  ) {
+    return this.quizService.update(q_id, content);
+  }
+
+  @ApiResponseWithBody(
+    HttpStatus.NO_CONTENT,
+    'クイズ削除',
+    'クイズ削除に成功しました。',
+  )
+  @UseRoleGuards([Role.ADMIN])
+  @Delete(':q_id')
+  remove(@Param('q_id', BigIntPipe) q_id: bigint) {
+    return this.quizService.remove(q_id);
+  }
+}
