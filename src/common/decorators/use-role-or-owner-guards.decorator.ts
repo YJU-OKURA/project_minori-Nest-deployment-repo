3f3482e734cfc3279656909b921779e285217d1b
Z00One <9000248@g.yju.ac.kr>
2024-04-10 22:59:57 +0900

    ✨ feat: Add id parameter to UseRoleOrOwnerGuards decorator
    Add an optional id parameter to the UseRoleOrOwnerGuards decorator for more flexibility in specifying the identifier.

--- src/common/decorators/use-role-or-owner-guards.decorator.ts
+++ src/common/decorators/use-role-or-owner-guards.decorator.ts
@@ -6,10 +6,12 @@ import { Key, MetaData } from './metadata.decorator';
 export const UseRoleOrOwnerGuards = (
   role: Role[],
   model: Prisma.ModelName,
+  id: string = 'id',
 ) => {
   return applyDecorators(
     MetaData<Prisma.ModelName>(Key.MODELS, model),
     MetaData<Role[]>(Key.ROLES, role),
+    MetaData<string>(Key.ID_PARAM, id),
     UseGuards(RoleOrOwnerGuard),
   );
 };
