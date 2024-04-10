c057a7d582ca32f553ce21f0956e157edf2682e8
Z00One <9000248@g.yju.ac.kr>
2024-04-10 22:13:20 +0900

    🚚 fix: Fix ClassUserModule import

--- src/modules/material/material.module.ts
+++ src/modules/material/material.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MaterialController } from './material.controller';
 import { MaterialService } from './material.service';
-import { AuthModule } from '@modules/auth/auth.module';
 import { UploadModule } from '@modules/material/upload/upload.module';
 import { PrismaModule } from '@modules/prisma/prisma.module';
 import { MaterialRepository } from './material.repository';
 import { LangchainModule } from '@modules/material/langchain/langchain.module';
+import { ClassUserModule } from '@modules/class-user/class-user.module';
 
 @Module({
   imports: [
-    AuthModule,
+    ClassUserModule,
     PrismaModule,
     UploadModule,
     LangchainModule,
