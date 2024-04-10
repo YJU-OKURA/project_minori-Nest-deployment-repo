ab37852ff8bcbdd89b2bf917a36dae3666bd1b7c
Z00One <9000248@g.yju.ac.kr>
2024-04-10 22:25:32 +0900

    ✅ refactor: Refactor class user repository's test code
    Update function names and variable names for clarity. Add new functions to retrieve class user information and multiple class users

--- src/modules/class-user/class-user.repository.spec.ts
+++ src/modules/class-user/class-user.repository.spec.ts
@@ -3,22 +3,8 @@ import { ClassUserRepository } from './class-user.repository';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('ClassUserRepository', () => {
-  let classUserService: ClassUserRepository;
+  let classUserRepository: ClassUserRepository;
   let prismaService: PrismaService;
-
-  const u_id = 3n;
-  const c_id = 3n;
-  const role = 'USER';
-  const nickname = 'test';
-  const is_favorite = true;
-
-  const classUserData = {
-    u_id,
-    c_id,
-    nickname,
-    is_favorite,
-    role,
-  };
 
   beforeEach(async () => {
     const module: TestingModule =
@@ -28,17 +14,37 @@ describe('ClassUserRepository', () => {
           {
             provide: PrismaService,
             useValue: {
-              class_user: {
-                findUnique: jest
-                  .fn()
-                  .mockResolvedValue(classUserData),
+              classUser: {
+                findUnique: jest.fn().mockResolvedValue({
+                  u_id: 1n,
+                  c_id: 1n,
+                  role: 'USER',
+                  nickname: 'test1',
+                  is_favorite: true,
+                }),
+                findMany: jest.fn().mockResolvedValue([
+                  {
+                    u_id: 1n,
+                    c_id: 1n,
+                    role: 'USER',
+                    nickname: 'test1',
+                    is_favorite: true,
+                  },
+                  {
+                    u_id: 2n,
+                    c_id: 1n,
+                    role: 'ADMIN',
+                    nickname: 'test2',
+                    is_favorite: true,
+                  },
+                ]),
               },
             },
           },
         ],
       }).compile();
 
-    classUserService = module.get<ClassUserRepository>(
+    classUserRepository = module.get<ClassUserRepository>(
       ClassUserRepository,
     );
     prismaService =
@@ -47,15 +53,56 @@ describe('ClassUserRepository', () => {
 
   it('should return class user', async () => {
     expect(
-      await classUserService.getClassUserInfo(u_id, c_id),
-    ).toEqual(classUserData);
+      await classUserRepository.getClassUserInfo(1n, 1n),
+    ).toEqual({
+      u_id: 1n,
+      c_id: 1n,
+      role: 'USER',
+      nickname: 'test1',
+      is_favorite: true,
+    });
     expect(
       prismaService.classUser.findUnique,
     ).toHaveBeenCalledTimes(1);
     expect(
       prismaService.classUser.findUnique,
     ).toHaveBeenCalledWith({
-      where: { u_id_c_id: { u_id, c_id } },
+      where: { u_id_c_id: { u_id: 1n, c_id: 1n } },
+    });
+  });
+
+  it('should return class users', async () => {
+    expect(
+      await classUserRepository.getClassUsersInfo(1n, 1, 2),
+    ).toEqual([
+      {
+        u_id: 1n,
+        c_id: 1n,
+        role: 'USER',
+        nickname: 'test1',
+        is_favorite: true,
+      },
+      {
+        u_id: 2n,
+        c_id: 1n,
+        role: 'ADMIN',
+        nickname: 'test2',
+        is_favorite: true,
+      },
+    ]);
+    expect(
+      prismaService.classUser.findMany,
+    ).toHaveBeenCalledWith({
+      where: { c_id: 1n },
+      skip: 0,
+      take: 2,
+      select: {
+        u_id: true,
+        nickname: true,
+      },
+      orderBy: {
+        u_id: 'asc',
+      },
     });
   });
 });
