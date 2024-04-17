be141b06aff76cb481df42ae0a569f762900966f
Z00One <9000248@g.yju.ac.kr>
2024-04-17 19:51:25 +0900

    🐛 fix: Remove unused ID_PARAM from metadata decorator

--- src/common/decorators/metadata.decorator.ts
+++ src/common/decorators/metadata.decorator.ts
@@ -3,7 +3,6 @@ import { SetMetadata } from '@nestjs/common';
 export enum Key {
   ROLES = 'roles',
   MODELS = 'models',
-  ID_PARAM = 'id',
 }
 
 export const MetaData = <T>(key: Key, models: T) =>
